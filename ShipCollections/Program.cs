478f938aace16802611734b30fae49458d7b8e78
Leon Akasaka <leon@wisdomsoft.jp>
2013-11-15 18:07:25 +0900

    抜錨だ！

---
+++ ShipCollections/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ships = Ship.LoadAll();
+            var types = ShipType.LoadAll();
+
+            //以下にデータ処理や演習問題を書きます。
+        }
+    }
+}
