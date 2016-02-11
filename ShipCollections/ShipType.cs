8d971bce95fb8841f2c83c056f084cb248c74538
Leon Akasaka <leon@wisdomsoft.jp>
2016-02-12 05:12:09 +0900

    データを Json 化した。
    Nuget で Newtonsoft.Json パッケージを追加。

--- ShipCollections/ShipType.cs
+++ ShipCollections/ShipType.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System.IO;
 
 namespace ShipCollections
 {
@@ -12,108 +13,12 @@ namespace ShipCollections
         /// <summary>
         /// 艦船の種類を表す名前です。
         /// </summary>
-        public String Name { get; set; }
+        public string Name { get; set; }
 
         public static ShipType[] LoadAll()
         {
-            var types = new[]
-            {
-                new ShipType
-                {
-                    Id = 1,
-                    Name = "海防艦",
-                },
-                new ShipType
-                {
-                    Id = 2,
-                    Name = "駆逐艦",
-                },
-                new ShipType
-                {
-                    Id = 3,
-                    Name = "軽巡洋艦",
-                },
-                new ShipType
-                {
-                    Id = 4,
-                    Name = "重雷装巡洋艦",
-                },
-                new ShipType
-                {
-                    Id = 5,
-                    Name = "重巡洋艦",
-                },
-                new ShipType
-                {
-                    Id = 6,
-                    Name = "航空巡洋艦",
-                },
-                new ShipType
-                {
-                    Id = 7,
-                    Name = "軽空母",
-                },
-                new ShipType
-                {
-                    Id = 8,
-                    Name = "戦艦",
-                },
-                new ShipType
-                {
-                    Id = 9,
-                    Name = "戦艦",
-                },
-                new ShipType
-                {
-                    Id = 10,
-                    Name = "航空戦艦",
-                },
-                new ShipType
-                {
-                    Id = 11,
-                    Name = "正規空母",
-                },
-                new ShipType
-                {
-                    Id = 12,
-                    Name = "超弩級戦艦",
-                },
-                new ShipType
-                {
-                    Id = 13,
-                    Name = "潜水艦",
-                },
-                new ShipType
-                {
-                    Id = 14,
-                    Name = "潜水空母",
-                },
-                new ShipType
-                {
-                    Id = 15,
-                    Name = "補給艦",
-                },
-                new ShipType
-                {
-                    Id = 16,
-                    Name = "水上機母艦",
-                },
-                new ShipType
-                {
-                    Id = 17,
-                    Name = "揚陸艦",
-                },
-                new ShipType
-                {
-                    Id = 18,
-                    Name = "装甲空母",
-                },
-                new ShipType
-                {
-                    Id = 19,
-                    Name = "工作艦",
-                },
-            };
+            var json = File.ReadAllText("Assets/ShipTypes.json");
+            var types = JsonConvert.DeserializeObject<ShipType[]>(json);
             return types;
         }
     }
